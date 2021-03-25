0212ca051cf1476736cbc610df44088122234b04
Varvarei Marius Razvan <mariusrazvan@protonmail.com>
2021-03-25 18:35:06 +0000

    Update game2.py
    Corrected some words

--- game2.py
+++ game2.py
@@ -1,8 +1,8 @@
 describe = print("""Hi!Let\'s play something cool:
-you told me every sentance you want,
-and i told you how many words have:""")
+you tell me any sentence you want
+and I will tell you how many words it contains:""")
 question = str(input())
 words = len(question.split())
-rezult = "You just told me a sentence of" +" " + str(words) + "  words"
+rezult = "You just told me a sentence made of" +" " + str(words) + "  words"
 print(rezult)
  
